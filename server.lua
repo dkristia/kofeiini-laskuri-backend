be605054b4988ffa6f086786518d524bb5350ee1
Daniel Kristiansson <daniel.i.kristiansson@gmail.com>
2024-05-27 22:21:40 +0300

    remove useless

--- server.lua
+++ server.lua
@@ -2,39 +2,26 @@
 
 local http = require("http")
 local url = require("url")
-
-local function multiples(t,suffix,prefix,min)
-  t,suffix,prefix=math.floor(t),suffix or "", prefix or ""
-  if t<(min or 0) then return "" end
-  if t==0 then return "" end
-  if t==1 then return prefix.."1 "..suffix end
-  return string.format("%s%i %ss",prefix,t,suffix)
-end
+local json = require("json")
 
 local function getCaffeine(obj) -- returns human readable advice as a string
-  local inp,hours = obj.amount, obj.time or 10
+  local inp, hours = obj.amount, obj.time or 10
   local reached = false
   local active = obj.previous and tonumber(obj.previous) or 0
-  if obj.previous and obj.threshold and active<tonumber(obj.threshold) then
-    return "It's time for coffee!"
-  end
+  local plot = {}
   local dt, hlin, hlout = 0.1, 0.75, 3
-  for t=0,hours,dt do
+  for t = 0, hours, dt do
     active = active + inp * (dt / hlin)
     inp = inp - inp * (dt / hlin)
     active = active - active * (dt / hlout)
-    if reached and not obj.time and obj.threshold and active<tonumber(obj.threshold) then
-      return string.format("Next coffee break in%s%s.",multiples(t,"hour"," ",1),multiples(t%1*60,"minute",t>1 and " and " or " "))
-    elseif not obj.time and obj.threshold and active>tonumber(obj.threshold) then
-      reached = true
-    end
+    table.insert(plot, {active=active, time=t})
   end
-  return "You don't need a coffee break."  --string.format("%.0fmg active at %.0f minutes", active, hours*60)
+  return json.encode(plot)   -- string.format("%.0fmg active at %.0f minutes", active, hours*60)
 end
 
 http.createServer(function(req, res)
-  local obj = url.parse(req.url,true).query
-  local ok, out = pcall(getCaffeine,obj)
+  local obj = url.parse(req.url, true).query
+  local ok, out = pcall(getCaffeine, obj)
   if ok then
     res:finish(out)
     return
@@ -43,4 +30,4 @@ http.createServer(function(req, res)
   end
   res.statusCode = 400
   res:finish("Bad Request")
-end):listen(8080)
+end):listen(1886)
